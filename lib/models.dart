5385bf38ff909b274407c87406f0a79250ee8f36
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-22 09:19:01 +0300

    Build HydroSync client: add lib/models.dart

---
+++ lib/models.dart
@@ -0,0 +1,6 @@
+class SessionUser {
+  final int id; final String name; final String email; final List<String> roles; final List<String> permissions;
+  SessionUser({required this.id,required this.name,required this.email,this.roles=const [],this.permissions=const []});
+  factory SessionUser.fromJson(Map<String,dynamic> j)=>SessionUser(id:(j['id'] as num?)?.toInt()??0,name:'\${j['name']??''}',email:'\${j['email']??''}',roles:(j['roles'] as List???[]).map((e)=>'$e').toList(),permissions:(j['permissions'] as List???[]).map((e)=>'$e').toList());
+}
+class ApiList { final List<Map<String,dynamic>> items; final int total; ApiList(this.items,this.total); }
