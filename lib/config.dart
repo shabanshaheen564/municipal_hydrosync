b56fc32010e776e33f664d7c969bacbe63f08db6
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-22 09:18:59 +0300

    Build HydroSync client: add lib/config.dart

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+class AppConfig {
+  static const String apiBaseUrl = String.fromEnvironment('API_BASE_URL', defaultValue: 'http://127.0.0.1:8000/api');
+  static const Duration requestTimeout = Duration(seconds: 25);
+}
