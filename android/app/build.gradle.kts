964c4139928eb85831b0e0f074692e9ca4d54a01
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-23 13:33:02 +0300

    fix: align Android NDK version with plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.municipal_hydrosync"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
