18619996edf0ecda60f0e83a4921c92a834f23c5
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-23 12:23:35 +0300

    chore: harden mobile production

--- lib/connectivity.dart
+++ lib/connectivity.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
@@ -10,23 +12,24 @@ class ConnectivityService {
 
   final _isOnline = ValueNotifier<bool>(true);
   late final Connectivity _connectivity;
+  StreamSubscription<List<ConnectivityResult>>? _subscription;
 
-  ValueNotifier<bool> get isOnline => _isOnline;
+  ValueListenable<bool> get isOnline => _isOnline;
 
   Future<void> init() async {
     _connectivity = Connectivity();
 
-    // Check initial state
     final result = await _connectivity.checkConnectivity();
     _isOnline.value = result.contains(ConnectivityResult.none) == false;
 
-    // Listen to changes
-    _connectivity.onConnectivityChanged.listen((result) {
+    await _subscription?.cancel();
+    _subscription = _connectivity.onConnectivityChanged.listen((result) {
       _isOnline.value = result.contains(ConnectivityResult.none) == false;
     });
   }
 
   void dispose() {
-    _isOnline.dispose();
+    _subscription?.cancel();
+    _subscription = null;
   }
 }
