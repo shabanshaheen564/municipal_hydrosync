44df1fc3ecb49d0aa92c28405a336d206d4dcd85
shaban shaheen <123385288+shabanshaheen564@users.noreply.github.com>
2026-09-26 13:33:11 +0300

    fix: raise Android min SDK for Firebase Messaging

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "ps.modb.water"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
